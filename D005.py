eb8d8e2737cdc2faf2a095b177a54f59bb9e2825
ajw123 <aw@broadinstitute.org>
2020-12-05 09:36:35 -0500

    Update D005.py

--- D005.py
+++ D005.py
@@ -1,25 +1,9 @@
 def get_seatID(passID):
-    seatRanges = {
-                    "rows":[0,127],
-                    "cols":[0,7]
-                 }
-    for bin in passID:
-        if bin in ["B","F"]:
-            id = "rows"
-        else:
-            id = "cols"
-            
-        if bin in ["B","R"]:
-            lRow = ((seatRanges[id][1] + 1) - seatRanges[id][0])/2 + seatRanges[id][0]
-            uRow = seatRanges[id][1]
-            seatRanges[id] = [lRow, uRow]
-        elif bin in ["F","L"]:
-            lRow = seatRanges[id][0]
-            uRow = ((((seatRanges[id][1] + 1) - seatRanges[id][0])/2) - 1) + seatRanges[id][0]
-            seatRanges[id] = [lRow, uRow]
-    seatID = seatRanges["rows"][0] * 8 + seatRanges["cols"][0]
+    rows = int(passID[:7].replace("B","1").replace("F","0"),base=2)
+    cols = int(passID[7:].replace("R","1").replace("L","0"),base=2)
+    seatID = rows * 8 + cols
     return seatID
-
+    
 seatIDArray = []
 
 with open("input.txt", "r") as f:
@@ -35,11 +19,10 @@ print(max(seatIDArray))
 #part 2
 seatIDArray.sort()
 prevSeat = min(seatIDArray)
-print(prevSeat)
+
 for seat in seatIDArray:
     if seat - prevSeat == 2:
         yourSeat = seat + 1
         break
     prevSeat = seat
-
 print(yourSeat)
